d84d9e10a478961a9f20cc63f68fb55c970fabd1
Danil Soloviev <danilsolo123@gmail.com>
2018-04-24 15:48:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,44 @@
 #coding:utf-8
 import telebot
-import sqlite3
 import logging
-import random
 import time
-import datetime
+import flask
 from telebot import types
 import botparts
+
+###############################################################
+API_TOKEN = '516231077:AAHz8hZQ71xHagXAZ1NsmJfHZSpZt2-1R_E'
+WEBHOOK_HOST = 'ip'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % API_TOKEN
+# logger = telebot.logger
+# telebot.logger.setLevel(logger.info)
+bot = telebot.TeleBot(API_TOKEN)
+app = flask.Flask(__name__)
+
+
+# Empty webserver index, return nothing, just http 200
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return ''
+
+
+# Process webhook calls
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+###############################################################
 
 
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)-3s]# %(levelname)-5s [%(asctime)s] %(message)s'
@@ -70,4 +102,19 @@ def inlin(call):
                           , reply_markup=botparts.questionkeyboard)
 
 
-bot.polling()
+###############################################################
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+time.sleep(1)
+
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+
+# Start flask server
+app.run(host=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT,
+        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+        debug=True)
